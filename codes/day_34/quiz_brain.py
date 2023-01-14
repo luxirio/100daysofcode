e12897b1ae3e7edba0914e9f0dda37cfe482048e
Gustavo <gustavo_h_marques@outlook.com>
2023-01-14 00:57:22 -0300

    final day_34

--- codes/day_34/quiz_brain.py
+++ codes/day_34/quiz_brain.py
@@ -12,19 +12,19 @@ class QuizBrain:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        self.current_question = self.question_list[self.question_number]
-        self.question_number += 1
-        return f"Q.{self.question_number}: {html.unescape(self.current_question.text)}"
-        #user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
-        #self.check_answer(user_answer)
+        if self.still_has_questions():
+            self.current_question = self.question_list[self.question_number]
+            self.question_number += 1
+            return f"Q.{self.question_number}: {html.unescape(self.current_question.text)}"
+            #user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
+            #self.check_answer(user_answer)
+        else:
+            return False
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- codes/day_34/ui.py
+++ codes/day_34/ui.py
@@ -3,6 +3,8 @@ from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
+RED_COLOR = "#A22B22"
+GREEN_COLOR = "#FDf22b"
 
 class QuizInterface():
     def __init__(self, quiz):
@@ -20,7 +22,7 @@ class QuizInterface():
         self.score.grid(row=0, column=1)
         
         # Generating canvas (where the question text is going to be)
-        self.canvas = Canvas(width=300, height=250)
+        self.canvas = Canvas(width=300, height=250, bg="white")
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
         self.canvas_text = self.canvas.create_text(150,125, 
                                                     text=quiz.current_question, 
@@ -29,11 +31,11 @@ class QuizInterface():
 
         # Now the buttons
         self.false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=self.false_image, command=self.get_question)
+        self.false_button = Button(image=self.false_image, command=self.false_pressed)
         self.false_button.grid(row=2, column=0)
 
         self.true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=self.true_image, command=self.get_question)
+        self.true_button = Button(image=self.true_image, command=self.true_pressed)
         self.true_button.grid(row=2, column=1)
 
         self.get_question()
@@ -41,6 +43,32 @@ class QuizInterface():
         self.window.mainloop()
 
     def get_question(self):
+        self.canvas.config(bg="white")
+        self.score.config(text=f'Score: {self.quiz.score}')
         q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.canvas_text, text=q_text)
+        if q_text:
+            self.canvas.itemconfig(self.canvas_text, text=q_text)
+        else:
+            self.window.quit()
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer(user_answer="True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer(user_answer="False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+
+            self.canvas.config(bg=GREEN_COLOR)
+            self.window.after(1000, func=self.get_question)
+        else:
+            self.canvas.config(bg=RED_COLOR)
+            self.window.after(1000, func=self.get_question)
+
+            
+
+
+
         
+
