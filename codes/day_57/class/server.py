7941769723a567126ba857a10090befe6434bebe
Gustavo <gustavo_h_marques@outlook.com>
2023-06-26 16:46:28 -0300

    day 57 finished

--- codes/day_57/class/server.py
+++ codes/day_57/class/server.py
@@ -18,11 +18,17 @@ def home(name):
     # API Fetch
     age_response = requests.get(AGE_ENDPOINT+str(name))
     gender_response = requests.get(GENDER_ENDPOINT+str(name))
-    
     age = age_response.json()['age']
     gender = gender_response.json()['gender']
 
     return render_template("index.html", year= CURRENT_YEAR, name=name, age=age, gender=gender)
+
+
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c7fecc37a5a8ef85eae7"
+    all_posts = requests.get(blog_url).json()
+    return render_template("blog.html", posts = all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
