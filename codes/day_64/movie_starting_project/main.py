dd85045292db555ebd2cb45e0b499556edb8d7f6
Gustavo <gustavo_h_marques@outlook.com>
2023-07-17 11:01:06 -0300

    finishing day_64

--- codes/day_64/movie_starting_project/main.py
+++ codes/day_64/movie_starting_project/main.py
@@ -6,7 +6,8 @@ from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired, Length
 import requests
 
-URL_ENDPOINT = "https://api.themoviedb.org/3/search/movie?query="
+QUERY_ENDPOINT = "https://api.themoviedb.org/3/search/movie?query="
+INFO_ENDPOINT = "https://api.themoviedb.org/3/movie/"
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGMyODUwMGMzMjUwMzc1M2I2ODhhZDk5M2YwOGM1MCIsInN1YiI6IjY0YjFhOGMzMjNkMjc4MDE0NTgzZmExZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H2ItwhmHR7tZUoihVI_wW6GIhoHbtIETsszihh4v6gY"
@@ -27,9 +28,9 @@ class Movie(db.Model):
     title = db.Column(db.String, unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False, unique=True)
-    review = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String, nullable=True)
     img_url = db.Column(db.String, nullable=False)
 
 class editReview(FlaskForm):
@@ -48,7 +49,10 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    all_movies = db.session.execute(db.select(Movie).order_by(Movie.ranking.desc())).scalars()
+    all_movies = db.session.execute(db.select(Movie).order_by(Movie.rating.desc())).scalars().all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = i+1
+    db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 @app.route('/edit', methods=['POST', 'GET'])
@@ -76,8 +80,7 @@ def add():
     form = addMovie()
     if form.validate_on_submit():
         movie_title = form.title.data
-        print(movie_title)
-        response = requests.get(f"{URL_ENDPOINT}{movie_title}", headers=headers)
+        response = requests.get(f"{QUERY_ENDPOINT}{movie_title}", headers=headers)
         movies = response.json()['results']
         return render_template('select.html', movie_list=movies)
     return render_template('add.html', form=form)
@@ -85,6 +88,25 @@ def add():
 @app.route('/select', methods=['GET', 'POST'])
 def select():
     return render_template('select.html')
+
+@app.route('/fetch', methods=['GET', 'POST'])
+def fetch():
+    movie_id = request.args.get('movie_id')
+    info_response = requests.get(f"{INFO_ENDPOINT}{movie_id}", headers=headers).json()
+    movie_poster_path = info_response['poster_path']
+
+    # Adding the movie to the database
+    new_movie = Movie(
+        title=info_response['title'],
+        year=info_response['release_date'].split('-')[0],
+        description=info_response['overview'],
+        img_url=f"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/{movie_poster_path}",
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+    print('This is the new recently added movie id:',new_movie.id)
+    return redirect(url_for('edit', id= new_movie.id))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
